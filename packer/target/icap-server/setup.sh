d9762aa314d64b70c400b328c03c5be6eae7065e
Pranay <pranaysahith7@gmail.com>
2021-06-11 14:07:32 +0000

    add sudo

--- packer/target/icap-server/setup.sh
+++ packer/target/icap-server/setup.sh
@@ -207,6 +207,6 @@ CREATE_OVA=${CREATE_OVA:-false}
 if [[ "$CREATE_OVA" == "true" ]]; then
 	echo $CREATE_OVA
 	curl -sSL https://raw.githubusercontent.com/vmware/cloud-init-vmware-guestinfo/master/install.sh | sudo sh -
-	rm -f /etc/cloud/cloud.cfg.d/99-DataSourceVMwareGuestInfo.cfg
-	sed -i "s/Ec2/Ec2, VMwareGuestInfo/g" /etc/cloud/cloud.cfg.d/90_dpkg.cfg
+	sudo rm -f /etc/cloud/cloud.cfg.d/99-DataSourceVMwareGuestInfo.cfg
+	sudo sed -i "s/Ec2/Ec2, VMwareGuestInfo/g" /etc/cloud/cloud.cfg.d/90_dpkg.cfg
 fi

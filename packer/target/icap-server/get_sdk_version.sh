68da2bbd0ea7894bee429c7e3712caa4500ff3c8
Pranay <pranaysahith7@gmail.com>
2021-06-13 10:30:41 +0000

    clone main branch by default

--- packer/target/icap-server/get_sdk_version.sh
+++ packer/target/icap-server/get_sdk_version.sh
@@ -4,7 +4,7 @@ get_sdk_version() {
     repo_name=$1
     image_tag=$2
     commit_sha=$(echo $image_tag | cut -d"-" -f2)
-    git clone https://github.com/$repo_name && pushd icap-request-processing && git checkout ${commit_sha}
+    git clone https://github.com/$repo_name --branch main && pushd icap-request-processing && git checkout ${commit_sha}
     submodule_status=$(git submodule status)
     commit_msg=$(git log -1 --format=%s lib)
     sdk_version=$(echo $submodule_status | grep  "[0-9]*\.[0-9]*" -o || true)
